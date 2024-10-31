7616d9728eb1fd9cb91006b7e48c2a4bf1f34fc0
Connor Apple <c-apple@onu.edu>
2024-10-31 09:11:23 -0400

    Completed P8-2

--- Laboratory8-2/Source.cpp
+++ Laboratory8-2/Source.cpp
@@ -1,12 +1,23 @@
 #include <iostream>
 #include <string>
+#include <sstream>
 using namespace std;
 
 int countWords(string str);
 
 int main(){
+    string input;
 
+    while (true) {
+        cout << "Enter a string or Q to quit: ";
+        getline(cin, input);
+        if (input == "Q") break;
+        int wordCount = countWords(input);
+        cout << "Word count: " << wordCount << endl;
+    }
+    return 0;
 }
+
 
 int countWords(string str) {
     int count = 0;
